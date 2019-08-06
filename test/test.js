8760316008da0accc46b1c21e9a4bd014885f0a6
James Hoekzema <james.hoekzema@monsanto>
2019-08-06 13:59:50 -0500

    sketch out more tests - James

--- test/test.js
+++ test/test.js
@@ -28,26 +28,77 @@ describe('Contact Service', () => {
     afterEach(reset);
 
 	describe('search', () => {
-        it('should return newly added contact', async () => {
-            const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+        describe('add event', () => {
+            it('should return newly added contact', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                
+                await flush();
+                const results = service.search('First');
+                
+                expect(results.length).to.equal(1);
+                expect(results[0].id).to.equal(contact.id);
+            });
             
-            await flush();
-            const results = service.search('First');
+            it('should return multiple matching contacts', async () => {
+                const contact1 = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                const contact2 = createContact({ firstName: 'First', lastName: 'Other', primaryPhoneNumber: '314-555-0001' });
+                
+                await flush();
+                const results = service.search('First');
+                
+                expect(results.length).to.equal(2);
+                expect(results.map(res => res.id)).includes(contact1.id);
+                expect(results.map(res => res.id)).includes(contact2.id);
+            });
             
-            expect(results.length).to.equal(1);
-            expect(results[0].id).to.equal(contact.id);
+            it('should not return a contact before retrieving all the details from the service', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                
+                const before = service.search('First');
+                await flush();
+                const after = service.search('First');
+                
+                expect(before.length).to.equal(0);
+                expect(after.length).to.equal(1);
+                expect(after[0].id).to.equal(contact.id);
+            });
+
+            it('should return all the required fields', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                
+                await flush();
+                const results = service.search('First');
+                
+                expect(results.length).to.equal(1);
+                expect(results[0]).to.deep.equal({
+                    // fields here
+                });
+            });
         });
-        
-        it('should return multiple matching contacts', async () => {
-            const contact1 = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
-            const contact2 = createContact({ firstName: 'First', lastName: 'Other', primaryPhoneNumber: '314-555-0001' });
-            
-            await flush();
-            const results = service.search('First');
-            
-            expect(results.length).to.equal(2);
-            expect(results.map(res => res.id)).includes(contact1.id);
-            expect(results.map(res => res.id)).includes(contact2.id);
+
+        describe('change event', () => {
+            it('should return the updated change to a name', async () => {
+
+            });
+        });
+
+        describe('remove event', () => {
+            it('should not return removed contacts', async () => {
+
+            });
+        });
+
+        describe('searchable fields', () => {
+            it('should be searchable by phone number', async () => {
+
+            });
+        });
+
+        describe('special search syntax (bonus)', () => {
+            // search by specific field (e.g. name:First role:Developer)
+            // search in specific time (e.g. from:2019-01-01 to:2019-01-01)
+            // search by regex (e.g. /^Dan\w*$/i)
+            // limit returned fields (e.g. return:name,role)
         });
     });
 });
