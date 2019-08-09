537d506250a3467da35d2dd1a84b57ea5dab9ba3
James Hoekzema <james.hoekzema@monsanto>
2019-08-09 13:20:22 -0500

    implement some of the more basic tests - James

--- test/test.js
+++ test/test.js
@@ -18,6 +18,24 @@ function createContact(props) {
     return contacts[id];
 }
 
+function changeContact(contact, props) {
+    Object.assign(contact, props);
+
+    for (let prop in props) {
+        changedContact(contact.id, prop, props[prop]);
+    }
+
+    return contact;
+}
+
+function removeContact(contact) {
+    delete contacts[contact.id];
+
+    removedContact(contact.id);
+
+    return contact;
+}
+
 describe('Contact Service', () => {
     let service;
 
@@ -63,28 +81,56 @@ describe('Contact Service', () => {
                 expect(after[0].id).to.equal(contact.id);
             });
 
-            it('should return all the required fields', async () => {
-                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+            it('should return the expected fields in the expected format', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000', emailAddress: 'first.last@mail.com', role: 'Cool Kid' });
+                const contact2 = createContact({ firstName: 'First', lastName: 'Last', nickName: 'Joe', primaryPhoneNumber: '314-555-0001', secondPhoneNumber: '+13145551234' });
                 
                 await flush();
                 const results = service.search('First');
                 
-                expect(results.length).to.equal(1);
+                expect(results.length).to.equal(2);
                 expect(results[0]).to.deep.equal({
-                    // fields here
+                    name: 'First Last',
+                    phones: ['(314) 555-0000'],
+                    email: 'first.last@mail.com',
+                    address: '',
+                    role: 'Cool Kid',
+                    id: contact.id
+                });
+                expect(results[1]).to.deep.equal({
+                    name: 'Joe Last',
+                    phones: ['(314) 555-0001', '(314) 555-1234'],
+                    email: '',
+                    address: '',
+                    role: 'Employee',
+                    id: contact2.id
                 });
             });
         });
 
         describe('change event', () => {
             it('should return the updated change to a name', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                
+                await flush();
+                changeContact(contact, { firstName: 'Changed' });
 
+                expect(service.search('First').length).to.equal(0);
+
+                const results = service.search('Changed');
+                expect(results.length).to.equal(1);
+                expect(results[0].id).to.equal(contact.id);
             });
         });
 
         describe('remove event', () => {
             it('should not return removed contacts', async () => {
+                const contact = createContact({ firstName: 'First', lastName: 'Last', primaryPhoneNumber: '314-555-0000' });
+                
+                await flush();
+                removeContact(contact);
 
+                expect(service.search('First').length).to.equal(0);
             });
         });
 
